294627e2fff26628e97b0ab2a4b80e4dac684ce6
markitics <m@rkmoriarty.com>
2020-08-16 19:38:42 +0100

    Add .env with podcast RSS feed, image and Google Analytics ID. Swap out rotating image with alternative placeholder landing page text. Add meta title.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const cover_art_url = "https://ssl-static.libsyn.com/p/assets/0/b/c/e/0bce06651e36264f/Invest_Like_The_Best_Podcast_Cover_1400x1400.jpg"
+const cover_art_url = process.env.REACT_APP_SERIES_COVER_ART_URL;
+const notion_url = process.env.REACT_APP_NOTION_URL;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={cover_art_url} className="App-logo" alt="logo" />
-        <h1>Invest Like the Best</h1>
-        <p>
-          We're live. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="landing">
+          <h1>The best place to learn about business and investing</h1>
+          <img src={cover_art_url} className="App-logo" alt="logo" />
+          <p className="subtle">
+            Our mission is to capture and share the best information for
+            business <strong>builders and investors</strong>.
+          </p>
+          <p>
+            Join our <strong>ever-evolving university</strong> for investors,
+            operators, and entrepreneurs.
+          </p>
+          <a
+            className="App-link"
+            href={notion_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Mark's ideas
+          </a>
+        </div>
       </header>
     </div>
   );
